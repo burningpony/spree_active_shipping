fb170b6a425bac666042f0987c88127bcbdcb04f
Russell Osborne <russosborn@gmail.com>
2015-01-11 09:30:58 -0500

    Check to ensure Spree Frontend Checkout and Orders checkout controller exis before attempting to extend them.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,11 +1,14 @@
 # handle shipping errors gracefully during checkout
-Spree::CheckoutController.class_eval do
+if Kernel.const_defined?('Spree::CheckoutController')
+  Spree::CheckoutController.class_eval do
 
-  rescue_from Spree::ShippingError, :with => :handle_shipping_error
+    rescue_from Spree::ShippingError, with: :handle_shipping_error
 
-  private
+    private
+
     def handle_shipping_error(e)
       flash[:error] = e.message
       redirect_to checkout_state_path(:address)
     end
+  end
 end
--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,11 +1,14 @@
 # handle shipping errors gracefully during order update
-Spree::OrdersController.class_eval do
+if Kernel.const_defined?('Spree::OrdersController')
+  Spree::OrdersController.class_eval do
 
-  rescue_from Spree::ShippingError, :with => :handle_shipping_error
+    rescue_from Spree::ShippingError, with: :handle_shipping_error
 
-  private
+    private
+
     def handle_shipping_error(e)
       flash[:error] = e.message
       redirect_back_or_default(root_path)
     end
+  end
 end
